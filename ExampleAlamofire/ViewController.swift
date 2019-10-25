a218c55345ce474a6013fe4745af04b1c5c92516
Pedro Moura <pedro.moura@socialbank.com.br>
2019-10-25 10:08:53 -0300

    Initial Commit

---
+++ ExampleAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleAlamofire
+//
+//  Created by Pedro Henrique de Moura on 25/10/19.
+//  Copyright © 2019 Pedro Moura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
